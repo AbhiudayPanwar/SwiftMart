8e16d227014fa02c5071ce666f2b7ba4488fde5b
Abhiuday Panwar <abhiudaypanwar2004@gmail.com>
2025-07-24 16:41:19 +0530

    Implemented Navbar

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,19 +1,19 @@
 import { createContext, useContext, useState } from "react";
-import {useNavigate} from 'react-router-dom';
 
 export const AppContext  = createContext();
 
 //Provider function
 export const AppContextProvider = ({children})=>{
 
-    const navigate = useNavigate();
     const [user, setUser] = useState(null);
     const [seller, setIsSeller] = useState(false);
+    const [showUserLogin, setShowUserLogin] = useState(false);
 
-    const value = {navigate, user, setUser, seller, setIsSeller}
+
+    const value = { user, setUser, seller, setIsSeller, showUserLogin, setShowUserLogin}
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
 }
 
-export const useAppContext = useContext(AppContext);
+export const useAppContext = () => useContext(AppContext);
